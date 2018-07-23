cd629cc912c95ce2b9e831686f96fb566d44ea1f
gaurav <gaurav@millipixels.com>
2018-07-23 10:19:44 +0530

    update composer

--- config/referral.php
+++ config/referral.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of questocat/laravel-referral package.
- *
- * (c) questocat <zhengchaopu@gmail.com>
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
-
 return [
     /*
      * Model class name of users.
--- database/migrations/add_referral_to_users_table.php
+++ database/migrations/add_referral_to_users_table.php
@@ -1,12 +1,4 @@
 <?php
-/*
- * This file is part of questocat/laravel-referral package.
- *
- * (c) questocat <zhengchaopu@gmail.com>
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
